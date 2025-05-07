731a0de3fd07c076414aff4be884ac0873af42b7
Gonzalo Bandeira <bandeira@fence.finance>
2025-05-07 17:00:02 +0200

    added dynamic search for spots

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import 'react-native-reanimated';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { PortalProvider } from '@gorhom/portal';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <StatusBar style="auto" />
-      <AppNavigator />
+      <PortalProvider>
+        <StatusBar style="auto" />
+        <AppNavigator />
+      </PortalProvider>
     </SafeAreaProvider>
   );
 }
