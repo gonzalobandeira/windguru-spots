6112040097bcda121a86522565519d4ab63cef4b
Gonzalo Bandeira <gonzalobandeira@gmail.com>
2025-05-14 16:13:52 +0200

    BAN-64: fixed export bug and increased version (#20)

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,4 +1,4 @@
 export const APP_STORE_LINK = 'https://apps.apple.com/es/app/windguruspots/id6745230519?l=en-GB';
 
 export const SHARE_FORECAST_MESSAGE = (spotId) =>
-  `Take a look at this forecast!\n\nCheck it out on Windguru: https://www.windguru.cz/${spotId}\n\nFound using Windguru Spots 📲\nDownload: ${APP_STORE_LINK}`; 
+  `Take a look at this forecast!\n\nCheck it out on Windguru: https://www.windguru.cz/${spotId}\n\nFound using Windguru Spots ${APP_STORE_LINK} 📲`; 
