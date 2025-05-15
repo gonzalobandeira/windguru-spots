6eaf1e1480e23351459b6b673cbc3160ad07d8ec
Gonzalo Bandeira <bandeira@fence.finance>
2025-05-15 15:17:44 +0200

    added auto update check

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,4 +1,15 @@
-export const APP_STORE_LINK = 'https://apps.apple.com/es/app/windguruspots/id6745230519?l=en-GB';
+// App Store ID
+export const APP_STORE_ID = '6745230519';
+export const COUNTRY_CODE = 'ES';
+export const APP_LANGUAGE = 'en-GB';
+
+export const APP_STORE_URL = `itms-apps://itunes.apple.com/app/id${APP_STORE_ID}`;
+export const ITUNES_LOOKUP_URL = `https://itunes.apple.com/lookup?id=${APP_STORE_ID}&country=${COUNTRY_CODE}`;
+export const APP_STORE_LINK = `https://apps.apple.com/es/app/windguruspots/id${APP_STORE_ID}?l=${APP_LANGUAGE}`;
 
 export const SHARE_FORECAST_MESSAGE = (spotId) =>
   `Take a look at this forecast!\n\nCheck it out on Windguru: https://www.windguru.cz/${spotId}\n\nFound using Windguru Spots ${APP_STORE_LINK} 📲`; 
+
+export const UPDATE_APP_AVAILABLE = 'A new version of Windguru Spots is available. Would you like to update now?';
+
+
