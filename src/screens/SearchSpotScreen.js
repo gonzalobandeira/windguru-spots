ceea8a1e1b64ec1d92e8bb93220cabe3e66aaa71
apb07 <alvaro@clever.gy>
2025-05-18 22:33:53 +0200

    refactor: move SearchSpotScreen styles to separate file

--- src/screens/SearchSpotScreen.js
+++ src/screens/SearchSpotScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import SpotSearch from '../components/SpotSearch';
 import { Colors } from '../constants/Styles';
 import AppScreen from '../components/AppScreen';
 import { MaterialIcons } from '@expo/vector-icons';
+import { styles } from '../styles/SearchSpotScreen.styles';
 
 const SearchSpotScreen = ({ navigation }) => {
   const handleSpotSelect = (spot) => {
@@ -29,32 +30,5 @@ const SearchSpotScreen = ({ navigation }) => {
     </AppScreen>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 24,
-    backgroundColor: '#fff',
-  },
-  backButton: {
-    marginBottom: 8,
-    alignSelf: 'flex-start',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 16,
-    color: Colors.text.primary,
-  },
-  manualButton: {
-    marginTop: 24,
-    alignItems: 'center',
-  },
-  manualButtonText: {
-    color: Colors.primary,
-    fontSize: 16,
-    textDecorationLine: 'underline',
-  },
-});
 
 export default SearchSpotScreen; 
